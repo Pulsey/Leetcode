7b9afc0797d3515999433c7313081ef199ee0d8a
Gagan Jain <gagan.pulsi@gmail.com>
2025-06-10 20:08:53 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (19.99%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -4,8 +4,8 @@ class Solution {
         dp[0] = 0;
         dp[1] = 0;
         int i=0;
-        if(cost.length == 0) return 0;
-        if(cost.length == 1) return 0;
+        if(cost.length == 0) return dp[0];
+        if(cost.length == 1) return dp[1];
         for(i = 2; i<= cost.length; i++){
             dp[i] = Math.min(cost[i-1] + dp[i-1], cost[i-2] + dp[i-2]);
         }
